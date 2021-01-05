86e88b610f7abdb73dd04892c77516ca8a7775c3
hzhao-talendbj <hzhao@talend.com>
2021-01-05 10:50:18 +0800

    fix compile error

--- main/plugins/org.talend.designer.business.diagram/src/org/talend/designer/business/model/business/diagram/edit/policies/BusinessReferenceConnectionEditPolicy.java
+++ main/plugins/org.talend.designer.business.diagram/src/org/talend/designer/business/model/business/diagram/edit/policies/BusinessReferenceConnectionEditPolicy.java
@@ -1,8 +1,8 @@
 package org.talend.designer.business.model.business.diagram.edit.policies;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.gef.EditPart;
-import org.eclipse.gef.GraphicalEditPart;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.commands.CompoundCommand;
@@ -12,7 +12,10 @@ import org.eclipse.gef.editpolicies.ConnectionEditPolicy;
 import org.eclipse.gef.requests.GroupRequest;
 import org.eclipse.gmf.runtime.diagram.core.commands.DeleteCommand;
 import org.eclipse.gmf.runtime.diagram.ui.commands.ICommandProxy;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
 import org.eclipse.gmf.runtime.diagram.ui.editparts.IGraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.l10n.DiagramUIMessages;
+import org.eclipse.gmf.runtime.diagram.ui.preferences.IPreferenceConstants;
 import org.eclipse.gmf.runtime.diagram.ui.requests.EditCommandRequestWrapper;
 import org.eclipse.gmf.runtime.diagram.ui.requests.GroupRequestViaKeyboard;
 import org.eclipse.gmf.runtime.emf.type.core.requests.DestroyElementRequest;
@@ -21,8 +24,6 @@ import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialogWithToggle;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.ui.internal.IPreferenceConstants;
-import org.talend.commons.ui.gmf.util.DiagramUIMessages;
 
 /**
  * @generated
