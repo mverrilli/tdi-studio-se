e69d8fb82aaabb7449e34fc63ab57cc78b3e3ef0
Oleksandr Vladyka <oleksandr.vladyka@synapse.com>
2021-02-12 11:58:50 +0200

    Fix(TUP-30413:Comma missing for tELTMap with multiple inputs, when aliases are used)

--- main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/DbGenerationManager.java
+++ main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/DbGenerationManager.java
@@ -1287,6 +1287,10 @@ public abstract class DbGenerationManager {
                 isELTDBMap = isELTDBMap(source);
             }
             if (isELTDBMap) {
+                if (commaCouldBeAdded) {
+                    appendSqlQuery(sb, DbMapSqlConstants.COMMA);
+                    appendSqlQuery(sb, DbMapSqlConstants.SPACE);
+                }
                 buildTableDeclaration(component, sb, inputTable);
             } else if (!aliasAlreadyDeclared.contains(inputTable.getName())) {
                 if (crCouldBeAdded) {
